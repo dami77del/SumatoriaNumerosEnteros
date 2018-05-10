15dce38e3375838311dbcfc1f27884c90ca85770
Damian <damian.deligdisch@davinci.edu.ar>
2018-05-10 17:57:19 -0300

    A ver

---
+++ SumatoriaNumerosEnteros/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumatoriaNumerosEnteros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int numero1, numero2, suma;
+            
+            Console.Write("ingrese el primer nùmero : ");
+           
+            numero1 = int.Parse(Console.ReadLine());
+          
+            Console.Write("ingrese el segundo número : ");
+          
+            numero2 = int.Parse(Console.ReadLine());
+      
+            suma = numero1 + numero2;
+          
+            Console.Write("la suma es : ", suma);
+            Console.ReadLine();        
+        }
+    }
+}
